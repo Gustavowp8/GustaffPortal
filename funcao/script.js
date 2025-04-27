588c82942a09a9418921ffe1afc9c32e4e10d0e6
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2025-04-27 12:38:07 -0300

    Finalizando o site

--- funcao/script.js
+++ funcao/script.js
@@ -1,3 +1,15 @@
 function msg(){
     alert('Indisponivel no momento');
 }
+
+function irInsta(){
+    location="https://www.instagram.com/gustaffdev/";
+}
+
+function irYou(){
+    location="https://www.youtube.com/@gustaffDev";
+}
+
+function irLink(){
+    location="https://www.linkedin.com/in/gustavo-ferreira-dos-santos-103317154/";
+}
